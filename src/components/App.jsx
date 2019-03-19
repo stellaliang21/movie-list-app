d45debb0f8281bed245db83f8ec261820a260ada
Stella Liang <stellaliang@Stellas-MacBook-Pro.local>
2019-03-18 21:24:11 -0700

    Implement watch toggle

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,13 +6,29 @@ import AddMovies from "./AddMovie.js"
 class App extends React.Component {
   constructor(props) {
     super(props)
-
     this.state = {
       movie: '',
       addMovie: '',
-      currentMovie: movies
+      currentMovie: movies //[{title: "bla", watch: "unwatched"}]
     }
   }
+
+
+  handleFindMovie(callback) {
+    $.get('https://api.themoviedb.org/3/search/movie', {
+      api_key: '2876f55117ee9e9fb74bcb3f8a36ad6b',
+      query: 'barbie'
+    })
+      .done((data) => {
+        console.log(data.items)
+      })
+      .fail((data) => {
+        console.log('fail')
+      });
+  }
+
+  
+
 
   handleChange(event) {
     this.setState({ 
@@ -36,16 +52,26 @@ class App extends React.Component {
   }
 
   handleAddMovieClick() {
-    console.log(1113)
     if (this.state.currentMovie.length === 0) {
       this.setState({
-        currentMovie: [{title: this.state.addMovie}]
+        currentMovie: [{title: this.state.addMovie, watch: true}]
       })
     } else {
       this.setState((prevState) => ({
-        currentMovie: [...prevState.currentMovie, {title: this.state.addMovie}]
+        currentMovie: [...prevState.currentMovie, {title: this.state.addMovie, watch: true}]
       }))
     }
+  }
+
+
+  toggleWatchBtn(clickedMovie) {
+    var movie = this.state.currentMovie.map((mov) => {
+      if (clickedMovie === mov.title) {
+        mov.watch = !mov.watch
+      }
+      return mov
+    });
+    this.setState({currentMovie: movie})
   }
 
 
@@ -63,7 +89,11 @@ class App extends React.Component {
         handleAddMovie={this.handleAddMovie.bind(this)}
         handleAddMovieClick={this.handleAddMovieClick.bind(this)}/>
       </div>
-      <div><MovieList movies={this.state.currentMovie} /></div>
+      <div>
+        <MovieList movies={this.state.currentMovie} 
+        toggleWatchBtn={this.toggleWatchBtn.bind(this)}/>
+      </div>
+      <button onClick={this.handleTest}></button>
     </div>
     )
   }
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -3,10 +3,12 @@ import MovieListEntry from "./MovieListEntry.js"
 var MovieList = (props) => (
   <div className="movie-list">
     {props.movies.map((movie, i) => 
-    <h4 key={i}> <MovieListEntry movie={movie}/> </h4>)}
+    <h4 key={i}> <MovieListEntry movie={movie}/> 
+    <button onClick={() => props.toggleWatchBtn(movie.title)}>
+    {movie.watch ? 'watched' : 'unwatched'}</button> </h4>)}
   </div>
 )
-  
+
 
 
 export default MovieList 
